6045f259964b148f95112f15df77f208b61ee582
aaaravv <aaravmishra619@gmail.com>
2026-03-20 15:09:53 +0530

    feat: implement HealthConnect permission handling in MainActivity
    - Added HealthConnectPermissionDelegate to manage permission contract results.
    - Updated onCreate method to set the permission delegate for proper functionality.

--- android/app/src/main/java/com/rapidcapsule/mobileapp/MainActivity.kt
+++ android/app/src/main/java/com/rapidcapsule/mobileapp/MainActivity.kt
@@ -6,12 +6,15 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import dev.matinzd.healthconnect.permissions.HealthConnectPermissionDelegate
 
 class MainActivity : ReactActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     super.onCreate(savedInstanceState)
+    // In order to handle permission contract results, we need to set the permission delegate.
+    HealthConnectPermissionDelegate.setPermissionDelegate(this)
   }
 
   /**
